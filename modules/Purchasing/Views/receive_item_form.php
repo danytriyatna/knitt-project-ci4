ccbea4ed9f7b32056ab94968a020e800354facee
Agung Sulaksana <sulaksana34@gmail.com>
2024-11-10 04:28:44 +0700

    feat: add UI module Receive Item

--- modules/Purchasing/Views/receive_item_form.php
+++ modules/Purchasing/Views/receive_item_form.php
@@ -1,7 +1,54 @@
 <?= $this->extend('template'); ?>
 
 <?= $this->section('modal') ?>
-
+<div id="modal-detail-item" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Form Detail Item</h5>
+        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group row">
+          <label class="control-label text-start text-md-end col-md-2 col-form-label" for="select_item">Item</label>
+          <div class="col-md-9">
+            <input type="text" id="select_item" name="select_item" class="form-control" placeholder="Terisi otomatis oleh sistem" value="W-001 WHITE NEW 001" disabled>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="control-label text-start text-md-end col-md-2 col-form-label" for="select_unit">Unit</label>
+          <div class="col-md-9">
+            <input type="text" id="select_unit" name="select_unit" class="form-control" placeholder="Terisi otomatis oleh sistem" value="KGM" disabled>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="control-label text-start text-md-end col-md-2 col-form-label" for="qty_item">Qty</label>
+          <div class="col-md-9">
+            <input type="text" id="qty_item" name="qty_item" class="form-control" placeholder="Ketikkan qty item" value="100">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="control-label text-start text-md-end col-md-2 col-form-label" for="select_warehouse">Warehouse</label>
+          <div class="col-md-9">
+            <select id="select_warehouse" name="select_warehouse" class="form-select select2" data-placeholder="-- Pilih Warehouse --">
+              <option value="1" selected>GUDANG UTAMA</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="control-label text-start text-md-end col-md-2 col-form-label" for="lot_no">Lot No.</label>
+          <div class="col-md-9">
+            <input type="text" id="lot_no" name="lot_no" class="form-control" placeholder="Ketikkan lot no." value="6458765432">
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+        <button type="button" class="btn btn-success" data-bs-dismiss="modal">Simpan</button>
+      </div>
+    </div>
+  </div>
+</div>
 <?= $this->endSection('modal') ?>
 
 <?= $this->section('content'); ?>
@@ -28,44 +75,62 @@
       <div class="card">
         <div class="card-body">
           <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-7">
               <div class="row">
                 <div class="col-sm-6">
                   <div class="form-group row">
-                    <label class="control-label text-start text-md-end col-md-3 col-form-label" for="cr_no">CR No.</label>
+                    <label class="control-label text-start text-md-end col-md-3 col-form-label" for="receive_no">Receive No.</label>
                     <div class="col-md-9">
-                      <input type="text" id="cr_no" name="cr_no" class="form-control" placeholder="Ketikkan nomor CR" value="CRC2410001">
+                      <input type="text" id="receive_no" name="receive_no" class="form-control" placeholder="Ketikkan nomor PO" value="POD2410001">
                     </div>
                   </div>
                 </div>
                 <div class="col-sm-6">
                   <div class="form-group row">
-                    <label class="control-label text-start text-md-end col-md-4 col-form-label" for="tgl_cr">CR Date</label>
+                    <label class="control-label text-start text-md-end col-md-4 col-form-label" for="tgl_receive">Receive Date</label>
                     <div class="col-md-8">
-                      <input type="text" id="tgl_cr" name="tgl_cr" class="form-control datepicker" placeholder="Pilih tanggal CR" value="01 Oktober 2024">
+                      <input type="text" id="tgl_receive" name="tgl_receive" class="form-control datepicker" placeholder="Pilih tanggal Receive" value="01 November 2024">
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-6">
                   <div class="form-group row">
-                    <label class="control-label text-start text-md-end col-md-2 col-form-label custom-col-md-2" for="select_buyer">Buyer</label>
-                    <div class="col-md-10">
-                      <select id="select_buyer" name="select_buyer" class="form-select select2" data-placeholder="-- Pilih Buyer --">
-                        <option value="1">Yusuf</option>
+                    <label class="control-label text-start text-md-end col-md-3 col-form-label" for="po_no">PO NO</label>
+                    <div class="col-md-9">
+                      <select id="po_no" name="po_no" class="form-select select2" data-placeholder="-- Pilih PO NO --">
+                        <option value="1" selected>POD24110001</option>
                       </select>
                     </div>
                   </div>
                 </div>
               </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group row">
+                    <label class="control-label text-start text-md-end col-md-3 col-form-label" for="tgl_ship"><span class="text-nowrap">Ship Date</label>
+                    <div class="col-md-9">
+                      <input type="text" id="tgl_ship" name="tgl_ship" class="form-control datepicker" placeholder="Pilih ship date" value="01 November 2024">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group row">
+                    <label class="control-label text-start text-md-end col-md-4 col-form-label" for="form_no">Form No.</label>
+                    <div class="col-md-8">
+                      <input type="text" id="form_no" name="form_no" class="form-control" placeholder="Ketikkan form no." value="">
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-sm-5">
               <div class="form-group row">
-                <label class="control-label text-start text-md-end col-md-3 col-form-label" for="select_payment_type">Payment Type</label>
-                <div class="col-md-9">
-                  <select id="select_payment_type" name="select_payment_type" class="form-select select2" data-placeholder="-- Pilih Payment Type --">
-                    <option value="1">BCA 0000000000 CITRAKNIT</option>
+                <label class="control-label text-start text-md-end col-md-2 col-form-label" for="select_vendor">Vendor</label>
+                <div class="col-md-10">
+                  <select id="select_vendor" name="select_vendor" class="form-select select2" data-placeholder="-- Pilih Vendor --">
+                    <option value="1" selected>Vedor Citraknitt 01</option>
+                    <option value="2">Vedor Citraknitt 02</option>
+                    <option value="3">Vedor Citraknitt 03</option>
                   </select>
                 </div>
               </div>
@@ -80,26 +145,29 @@
                 <table class="table table-striped">
                   <thead>
                     <tr>
-                      <th>SI NO.</th>
-                      <th>SI DATE</th>
-                      <th>DUE DATE</th>
-                      <th>INV. AMOUNT</th>
-                      <th>PAID</th>
-                      <th>REMAINING AMOUNT</th>
-                      <th>PAYMENT AMOUNT</th>
+                      <th style="min-width: 95px; width: 95px;">
+                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal-detail-item"><i class="fa fa-plus"></i></button>
+                      </th>
+                      <th>ITEM CODE</th>
+                      <th>ITEM DESCRIPTION</th>
+                      <th>QTY</th>
+                      <th>UNIT</th>
+                      <th>WAREHOUSE</th>
+                      <th>LOT NO</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td>SIV24100001</td>
-                      <td>01-10-2024</td>
-                      <td>31-10-2024</td>
-                      <td class="text-nowrap">1.800.000,00</td>
-                      <td class="text-nowrap">0,00</td>
-                      <td class="text-nowrap">1.800.000,00</td>
                       <td>
-                        <input type="text" class="form-control form-idr" value="1800000">
+                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#modal-detail-item"><i class="fa fa-edit"></i></button>
+                        <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
                       </td>
+                      <td>W-001</td>
+                      <td>WHITE NEW 001</td>
+                      <td>100</td>
+                      <td>KGM</td>
+                      <td>GUDANG UTAMA</td>
+                      <td>6458765432</td>
                     </tr>
                   </tbody>
                 </table>
@@ -109,10 +177,10 @@
 
               <div class="row">
                 <div class="col-sm-10">
-                  <a href="trans/customer-receipt" class="btn btn-default m-e-5">
+                  <a href="purchasing/receive-item" class="btn btn-default m-e-5">
                     <span class="fa fa-arrow-left"></span> Kembali
                   </a>
-                  <a href="trans/customer-receipt" class='btn btn-success'>
+                  <a href="purchasing/receive-item" class='btn btn-success'>
                     <span class="fa fa-save"></span> Simpan
                   </a>
                 </div>
--- modules/Purchasing/Views/receive_item_list.php
+++ modules/Purchasing/Views/receive_item_list.php
@@ -27,36 +27,20 @@
     <div class="col-lg-12">
       <div class="card">
         <div class="card-body">
-          <div class="row">
-            <div class="col-sm-3">
-              <div class="form-group m-b-0 d-flex align-items-center">
-                <label class="control-label text-start text-md-end m-e-8" for="filter_status">Status</label>
-                <select id="filter_status" name="filter_status" class="form-control custom-select select2">
-                  <option value="0">All</option>
-                  <option value="1">Draft</option>
-                  <option value="2">Process</option>
-                  <option value="3">Done</option>
-                </select>
-              </div>
-            </div>
-          </div>
-          <hr>
           <div class="table-responsive">
             <table class="table table-striped datatable">
               <thead>
                 <tr>
                   <th style="min-width: 105px; width: 105px;">
-                    <a href="trans/customer-receipt/form" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i> Tambah</a>
+                    <a href="purchasing/receive-item/form" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i> Tambah</a>
                   </th>
-                  <th>CR NO.</th>
-                  <th>CR DATE</th>
-                  <th>SI NO.</th>
-                  <th>BUYER</th>
-                  <th>SI AMOUNT</th>
-                  <th>PAID AMOUNT</th>
-                  <th>PAYMENT</th>
-                  <th>REMAIN AMOUNT</th>
-                  <th>DO STATUS</th>
+                  <th>RECEIVE NO.</th>
+                  <th>RECEIVE DATE</th>
+                  <th>PO NO.</th>
+                  <th>SHIP DATE</th>
+                  <th>FORM NO.</th>
+                  <th>VENDOR NAME</th>
+                  <th>QTY</th>
                 </tr>
               </thead>
               <tbody>
@@ -69,23 +53,19 @@
                         <i class="fas fa-cog"></i> Aksi <span class="caret"></span>
                       </button>
                       <ul class="dropdown-menu dropdown-menu-act" role="menu">
-                        <li><a href="trans/customer-receipt/form" title="Edit"><i class="fa fa-fw fa-edit"></i> Edit</a></li>
+                        <li><a href="purchasing/receive-item/form" title="Edit"><i class="fa fa-fw fa-edit"></i> Edit</a></li>
                         <li><a href="javascript:void(0)" title="Hapus"><i class="fa fa-fw fa-trash"></i> Hapus</a>
                         </li>
                       </ul>
                     </div>
                   </td>
-                  <td>CRC24100001</td>
-                  <td>06-10-2024</td>
-                  <td>SIV24100001</td>
-                  <td>YUSUF</td>
-                  <td class="text-nowrap">1.800.000,00</td>
-                  <td class="text-nowrap">0,00</td>
-                  <td class="text-nowrap">1.800.000,00</td>
-                  <td class="text-nowrap">0,00</td>
-                  <td>
-                    <span class="badge bg-secondary">DRAFT</span>
-                  </td>
+                  <td>RIT24100001</td>
+                  <td>01-11-2024</td>
+                  <td>POD24110001</td>
+                  <td></td>
+                  <td></td>
+                  <td>Vendor Citraknitt 01</td>
+                  <td>0</td>
                 </tr>
                 <?php endfor; ?>
               </tbody>
