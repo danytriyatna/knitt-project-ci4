de526f0c573f1f50546f4b57b0d1f7a3c920dd63
indra <indrazitmegandana53@gmail.com>
2024-11-30 08:48:45 +0700

    feat: add laporan stock card

---
+++ modules/Laporan/Controllers/LaporanStockCard.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Modules\Laporan\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\FileModel;
+use Modules\Referensi\Models\BarangModel;
+use Modules\Referensi\Models\JenisBarangModel;
+use Modules\Referensi\Models\SatuanModel;
+use Modules\Transaction\Models\IncomingGoodsModel;
+use Modules\Referensi\Models\GudangModel;
+use Modules\Laporan\Models\LaporanStockCardModel;
+
+class LaporanStockCard extends BaseController
+{
+    protected $mBarang;
+    protected $mJenisBarang;
+    protected $mSatuan;
+    protected $mBarangMasuk;
+    protected $mGudang;
+    protected $mLaporan;
+
+    protected $views = '\Modules\Laporan\Views';
+    protected $urlv  = 'laporan/stock-card';
+
+    function __construct()
+    {
+        $this->MOD_ALIAS = "MOD_LAPORAN_STOCK_CARD";
+        $this->mBarang = new BarangModel();
+        $this->mJenisBarang = new JenisBarangModel();
+        $this->mSatuan = new SatuanModel();
+        $this->mBarangMasuk = new IncomingGoodsModel();
+        $this->mGudang = new GudangModel();
+        $this->mLaporan = new LaporanStockCardModel();
+        $this->files  = new FileModel();
+    }
+
+    public function index()
+    {
+
+        if (!$this->auth->loggedIn()) {
+            return redirect()->to('/auth/login');
+        }
+
+        $this->data['titlehead'] = "Laporan Stock Card ";
+
+        return view($this->views . '\laporan_stock_card', $this->data);
+    }
+
+
+    public function getDataLaporanStockCard()
+    {
+
+        $build_array = [];
+        $build_array["code"] = 200;
+        $build_array["status"] = false;
+
+        $idBarang = $this->request->getGet('filter_barang_id');
+        $idBarang = decrypt($idBarang);
+        $tgl_mulai = $this->request->getGet('tgl_mulai');
+        $tgl_akhir = $this->request->getGet('tgl_akhir');
+
+        $resData = $this->mLaporan->getLaporanStockCard($idBarang, $tgl_mulai, $tgl_akhir);
+
+        $build_array["message"] = "Data ditemukan";
+        $build_array["data"] =  !empty($resData) ? $resData : [];
+        $build_array["status"] = true;
+
+        return $this->response->setJSON($build_array);
+    }
+}
