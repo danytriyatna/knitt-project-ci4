d75f2efa88df9976bc4b6579e06dd5f4ce22dbe4
Egy Ihza Madhani <egyijaa@gmail.com>
2025-11-03 00:56:28 +0700

    ketinggalan progress

--- app/Models/Mdashboard.php
+++ app/Models/Mdashboard.php
@@ -92,7 +92,7 @@ class Mdashboard extends Model
         $builder = $this->db->table("v_traking_order_sample tbl");
 
         $builder->select("tbl.trans_id, tbl.trans_kode, tbl.tgl_transaksi, tbl.id_konsumen, tbl.nama, tbl.keterangan, tbl.tgl_deadline, tbl.qty, tbl.style, tbl.deskripsi, tbl.tipe, 
-                          tbl.kode_prod, tbl.id_prod, tbl.qty_prod, 
+                          tbl.kode_prod, tbl.id_prod, tbl.qty_prod, nilai_pembayaran,
                           tbl.kode_dev, tbl.id_dev, tbl.qty_kirim, rk.id_walkorder, tbl.file_name, tbl.uang_dp, tbl.harga_total, tbl.nilai_invoice");
         
         $builder->join("trans_produksi rk", "rk.id = tbl.id_prod", "left");
