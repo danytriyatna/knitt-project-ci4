a645cd1febff2be11ffc3d13920abce39d1cd1f7
Egy Ihza Madhani <egyijaa@gmail.com>
2026-06-28 15:14:16 +0700

    fix typo BTK

--- modules/Transaction/Models/BarangKeluarDetailModel.php
+++ modules/Transaction/Models/BarangKeluarDetailModel.php
@@ -29,7 +29,7 @@ class BarangKeluarDetailModel extends \App\Models\PrModel
         // $builder->join($this->tblTrxLots . " gbx", "uk.lot_no = gbx.lot_no AND gbx.id_gudang = $params[id_gudang] ", "inner");
 
         // $builder->select("uk.id,uk.id_header,uk.qty,uk.lot_no,gbx.id as lot_id, uk.id_barang,fbx.nama_satuan as nama_unit, ebx.kode_barang, ebx.nama_barang, uk.price");
-        $builder->select("uk.id,uk.id_header,uk.qty,uk.lot_no, uk.lot_id,uk.id_barang,fbx.nama_satuan as nama_unit, rp.pack_name, uk.pack_id, ebx.kode_barang, ebx.nama_barang, uk.price, tbh.no_ref_transfer, tbh.no_ref_wo");
+        $builder->select("uk.id,uk.id_header,uk.qty,uk.lot_no, uk.lot_id,uk.id_barang,fbx.nama_satuan as nama_unit, rp.pack_name, uk.pack_id, ebx.kode_barang, ebx.nama_barang, uk.price, tbh.no_ref_trf, tbh.no_ref_wo");
 
         if (!empty($params['id_header'])) {
             $builder->where('uk.id_header', $params['id_header']);
