bb2c822e67c74f29a5eddfe27ee2e8d9928cd3fc
danytriyatna <56550570+danytriyatna@users.noreply.github.com>
2026-07-29 09:47:56 +0700

    fix(trans): fix missing ref_pack table data replication and safeguard id_gudang parameter in ItemTransfer

--- modules/Transaction/Models/ItemTransferDetailModel.php
+++ modules/Transaction/Models/ItemTransferDetailModel.php
@@ -29,7 +29,8 @@ class ItemTransferDetailModel extends \App\Models\PrModel
         $builder->join($this->tblBarang . " ebx", "uk.id_barang = ebx.id", "inner");
         $builder->join($this->tblSatuan . " fbx", "ebx.id_satuan = fbx.id", "inner");
         $builder->join("trans_walkorder wo", "uk.kode_walkorder = wo.kode_walkorder", "left");
-        $builder->join($this->tblTrxLots . " gbx", "uk.lot_id = gbx.id AND gbx.id_gudang = $params[id_gudang]", "left");
+        $idGudang = !empty($params['id_gudang']) ? (int)$params['id_gudang'] : 0;
+        $builder->join($this->tblTrxLots . " gbx", "uk.lot_id = gbx.id AND gbx.id_gudang = {$idGudang}", "left");
         $builder->join($this->tblPack . " hbx", "uk.pack_id = hbx.id", "left");
         $builder->select("uk.id,gbx.id as lot_id,uk.id_header,uk.qty,uk.lot_no, uk.id_barang,
                           fbx.nama_satuan as nama_unit, ebx.kode_barang, ebx.nama_barang, 
